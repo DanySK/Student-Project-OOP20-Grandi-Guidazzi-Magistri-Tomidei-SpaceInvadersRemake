eede78df1e5571cce77863ae4b326f98ee41aad2
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-04-08 19:10:25 +0200

    implemented boss bullet movement

--- src/model/entities/BossBullet.java
+++ src/model/entities/BossBullet.java
@@ -1,5 +1,24 @@
 package model.entities;
 
-public class BossBullet {
+import model.entitiesutil.EntityDirections;
+
+public class BossBullet extends Entity {
+
+	private EntityDirections direction;
+
+	@Override
+	protected void updateEntityMovement() {
+		switch(this.direction) {
+			case DOWN_LEFT:
+				this.getMove().moveDownLeft(this);
+				break;
+			case DOWN_RIGHT:
+				this.getMove().moveDownRight(this);
+				break;
+			default:
+				this.getMove().moveDown(this);
+				break;
+		}
+	}
 
 }
