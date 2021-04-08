a662032cc339cc898564e8451d6b96fc3c44b963
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-04-08 19:14:57 +0200

    implemented boss bullet costructor

--- src/model/entities/BossBullet.java
+++ src/model/entities/BossBullet.java
@@ -1,10 +1,28 @@
 package model.entities;
 
+import java.util.List;
+
+import graphics.GraphicsComponentAwt;
 import model.entitiesutil.EntityDirections;
+import model.physics.EntityMovementImpl;
+import util.Pair;
+
 
 public class BossBullet extends Entity {
 
+	private final int BULLET_INITIAL_WIDTH = 0;
+	private final int BULLET_INITIAL_HEIGHT = 0;
+	private final int BULLET_INITIAL_MU_X = 0;
+	private final int BULLET_INITIAL_MU_Y = 0;
+
+	private List<String> bulletStrImgs;
 	private EntityDirections direction;
+
+	protected BossBullet(Pair<Integer,Integer> pos, List<String> strImg) {
+		this.create(pos, this.BULLET_INITIAL_WIDTH, this.BULLET_INITIAL_HEIGHT, this.BULLET_INITIAL_MU_X,
+				this.BULLET_INITIAL_MU_Y, this.bulletStrImgs, new GraphicsComponentAwt(this.bulletStrImgs),
+				new EntityMovementImpl());
+	}
 
 	@Override
 	protected void updateEntityMovement() {
