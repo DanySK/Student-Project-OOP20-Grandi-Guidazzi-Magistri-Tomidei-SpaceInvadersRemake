b99f36e5895224be0c559b4beff577ec62cedd17
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-04-18 15:43:52 +0200

    added minor changes to the code and, for each method the description of its behavior

--- src/model/physics/EntityMovement.java
+++ src/model/physics/EntityMovement.java
@@ -2,19 +2,52 @@ package model.physics;
 
 import model.entitiesutil.Entity;
 
+/**
+ * Interface that represent {@link Entity}'s movements
+ */
 public interface EntityMovement {
 
+	/**
+	 * Method that move the {@link Entity} to the left
+	 * 
+	 * @param e	is the {@link Entity} that needs to be moved
+	 */
 	public void moveLeft(Entity e);
 
+	/**
+	 * Method that move the {@link Entity} to the right
+	 * 
+	 * @param e	is the {@link Entity} that needs to be moved
+	 */
 	public void moveRight(Entity e);
 
+	/**
+	 * Method that move the {@link Entity} up
+	 * 
+	 * @param e	is the {@link Entity} that needs to be moved
+	 */
 	public void moveUp(Entity e);
 
+	/**
+	 * Method that move the {@link Entity} down
+	 * 
+	 * @param e	is the {@link Entity} that needs to be moved
+	 */
 	public void moveDown(Entity e);
 
-	public void moveDownLeft(Entity e);
+	/**
+	 * Method that move the {@link Entity} to the bottom left
+	 * 
+	 * @param e	is the {@link Entity} that needs to be moved
+	 */
+	public void moveBottomLeft(Entity e);
 
-	public void moveDownRight(Entity e);
+	/**
+	 * Method that move the {@link Entity} to the bottom right
+	 * 
+	 * @param e	is the {@link Entity} that needs to be moved
+	 */
+	public void moveBottomRight(Entity e);
 
 
 }
