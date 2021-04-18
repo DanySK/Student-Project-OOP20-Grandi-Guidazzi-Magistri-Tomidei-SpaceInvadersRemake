a23754d3f46b01901507d24bf7548cd2b4b254ed
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-04-18 15:17:37 +0200

    added for each Boss2's method the description of its behavior, and minor changes to the code

--- src/model/entities/Boss2.java
+++ src/model/entities/Boss2.java
@@ -3,20 +3,22 @@ package model.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import graphics.GraphicsComponentAwt;
+import graphics.EntityGraphicsImpl;
 import model.entitiesutil.BossState;
 import model.entitiesutil.Enemy;
 import model.entitiesutil.EntityDirections;
 import model.physics.EntityMovementImpl;
 import util.Pair;
 
+/**
+ * {@link Enemy} boss with a lot of life but that moves slowly
+ */
 public class Boss2 extends Enemy{
 
 	private final int INITIAL_WIDTH = 0;
 	private final int INITIAL_HEIGHT = 0;
 	private final int INITIAL_MU_X = 0;
 	private final int INITIAL_MU_Y = 0;
-	private final int SPEED = 0;
 	private final int HITS_2ND_PHASE = 0;
 	private final int MAX_HITS = 0;
 
@@ -25,11 +27,16 @@ public class Boss2 extends Enemy{
 
 	private List<String> bulletStrImg;
 
+	/**
+	 * {@link Enemy} boss with a lot of life but that moves slowly
+	 * 
+	 * @param pos is the initial position of this entity
+	 */
 	public Boss2(Pair<Integer, Integer> pos) {
 		this.strImg = new ArrayList<>();
 		this.strImg.add("");
-		this.create(EnemyType.BOSS, pos, this.INITIAL_WIDTH, this.INITIAL_HEIGHT, this.INITIAL_MU_X, this.INITIAL_MU_Y,
-				this.MAX_HITS, this.strImg, EntityDirections.DOWN, new GraphicsComponentAwt(this.strImg), new EntityMovementImpl());
+		this.create(EntityType.BOSS, pos, this.INITIAL_WIDTH, this.INITIAL_HEIGHT, this.INITIAL_MU_X, this.INITIAL_MU_Y,
+				this.MAX_HITS, this.strImg, EntityDirections.DOWN, new EntityGraphicsImpl(this.strImg), new EntityMovementImpl());
 		this.state = BossState.NORMAL;
 
 		this.bulletStrImg = new ArrayList<>();
@@ -38,18 +45,27 @@ public class Boss2 extends Enemy{
 		this.bulletStrImg.add("");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void changeDirection() {
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	protected void updateEntityMovement() {
+	public void updateEntityPos() {
 		this.changeState();
 		if(this.state.equals(BossState.UPSET)) {
-			this.getMovement().moveDown(this);
+			this.getMovementImpl().moveDown(this);
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void shot() {
 		/*(new BossBullet(new Pair<>(this.getX() + this.getWidth()/4 - 1, 
@@ -58,6 +74,9 @@ public class Boss2 extends Enemy{
 				this.getY() + this.getHeight()), this.bulletStrImg));*/
 	}
 
+	/**
+	 * Change the state of the boss after it took too many hits 
+	 */
 	private void changeState() {
 		if(this.getHits() >= this.HITS_2ND_PHASE) {
 			this.state = BossState.UPSET;
