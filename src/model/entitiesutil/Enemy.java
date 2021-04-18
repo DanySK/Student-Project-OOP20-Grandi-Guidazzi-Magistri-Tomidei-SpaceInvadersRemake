8f92e3f2bb33f50bc788475554e739fe88739630
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-04-18 16:30:05 +0200

    added for each Enemy's method the description of its behavior, and added minor changes to the code

--- src/model/entitiesutil/Enemy.java
+++ src/model/entitiesutil/Enemy.java
@@ -3,33 +3,40 @@ package model.entitiesutil;
 import java.awt.Graphics;
 import java.util.List;
 
-import graphics.EntitiesGraphics;
+import graphics.EntityGraphics;
 import model.physics.EntityMovement;
 import util.Pair;
 
-
+/**
+ * {@link Entity} that Hero should kill
+ */
 public abstract class Enemy implements Entity{
-
-	public enum EnemyType{
-
-
-		GENERIC_ENEMY,
-
-
-		BOSS
-	}
 
 	private Pair<Integer,Integer> pos;
 	private int width, height, muX, muY, hit, maxHits;
 	private List<String> strImgs;
-	private EntitiesGraphics graphics;
+	private EntityGraphics graphics;
 	private EntityMovement move;
 	private EntityDirections direction;
 	private EntityType entityType;
-	private EnemyType enemyType;
 
-	protected void create(EnemyType type, Pair<Integer,Integer> pos, int width,int height, int muX, int muY, int maxHits,
-			List<String> strImg, EntityDirections dir, EntitiesGraphics graph, EntityMovement move) {
+	/**
+	 * Create a new {@link Enemy}
+	 * 
+	 * @param type		is the {@link EnemyType}
+	 * @param pos		is the initial position of the {@link Enemy}
+	 * @param width		is the initial width of the {@link Enemy}
+	 * @param height	is the initial height of the {@link Enemy}
+	 * @param muX		is the initial movement unit of the {@link Enemy} along x-axis
+	 * @param muY		is the initial movement unit of the {@link Enemy} along y-axis
+	 * @param maxHits	is the max number of hits that {@link Enemy} can take before dying
+	 * @param strImg	is the images's path of the {@link Enemy}
+	 * @param dir		is the initial direction of the {@link Enemy}
+	 * @param graph		is the {@link EntityGraphics} implementation
+	 * @param move		is {@link EntityMovement} implementation
+	 */
+	protected void create(EntityType type, Pair<Integer,Integer> pos, int width,int height, int muX, int muY, int maxHits,
+			List<String> strImg, EntityDirections dir, EntityGraphics graph, EntityMovement move) {
 		this.width = width;
 		this.height = height;
 		this.pos = pos;
@@ -41,128 +48,195 @@ public abstract class Enemy implements Entity{
 		this.direction = dir;
 		this.hit = 0;
 		this.maxHits = maxHits;
-		this.entityType = EntityType.ENEMY;
-		this.enemyType = type;
-		
+		this.entityType = type;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Pair<Integer, Integer> getPos() {
 		return pos;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setPos(Pair<Integer, Integer> pos) {
 		this.pos = pos;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getX() {
 		return this.pos.getX();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getY() {
 		return this.pos.getY();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setX(int x) {
 		this.pos.setX(x);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setY(int y) {
 		this.pos.setY(y);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getWidth() {
 		return this.width;
 	}
 
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getHeight() {
 		return this.height;
 	}
 
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getMuX() {
 		return muX;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setMuX(int mux) {
 		this.muX = mux;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getMuY() {
 		return muY;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setMuY(int muy) {
 		this.muY = muy;
 	}
 
+	/**
+	 * Return the current number of hits that {@link Enemy} take
+	 * 
+	 * @return an integer which represent the current number of hits that {@link Enemy} take
+	 */
 	public int getHits() {
 		return this.hit;
 	}
 
+	/**
+	 * Increment the current number of hits that {@link Enemy} take
+	 */
 	public void hit() {
 		this.hit++;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public EntityMovement getMovement() {
+	public EntityMovement getMovementImpl() {
 		return this.move;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public EntityDirections getDirection() {
 		return this.direction;
 	}
 
 
+	/**
+	 * Update the direction of the {@link Enemy}
+	 * 
+	 * @param dir is the new direction of the {@link Enemy}
+	 */
 	protected void setDirection(EntityDirections dir) {
 		this.direction = dir;
 	}
 
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean isLife() {
 		return this.hit >= this.maxHits;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public List<String> getStrImgs() {
 		return strImgs;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void updateEntity(Graphics g, Entity e) {
-		this.updateEntityMovement();
 		this.graphics.updateGraphics(g, e);
 	}
 
-	protected abstract void updateEntityMovement();
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public abstract void updateEntityPos();
 
 
+	/**
+	 * Invert the direction of the {@link Enemy}
+	 */
 	public abstract void changeDirection();
 
 
+	/**
+	 * Create new {@link Bullet} according to the type of the {@link Enemy}
+	 */
 	public abstract void  shot();
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public EntityType getEntityType() {
 		return this.entityType;
-	}
-
-	public EnemyType getEnemyType() {
-		return this.enemyType;
 	}
 }
 
