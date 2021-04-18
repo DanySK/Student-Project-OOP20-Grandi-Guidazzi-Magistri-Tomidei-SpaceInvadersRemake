f0858790388483fa74eb9405392ccdc802a918bc
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-04-18 19:57:48 +0200

    added for each EntityGraphics's method a descrition of its behavior

--- src/graphics/EntityGraphics.java
+++ src/graphics/EntityGraphics.java
@@ -4,10 +4,25 @@ import java.awt.Graphics;
 
 import model.entitiesutil.Entity;
 
-public interface EntitiesGraphics {
+/**
+ * Interface for {@link Entity} update
+ */
+public interface EntityGraphics {
 
+	/**
+	 * Update the entity's graphics 
+	 * 
+	 * @param g is the type of graphics used for draw the {@link Entity}'s image
+	 * @param e is the {@link Entity} that need to be updated
+	 */
 	public void updateGraphics(Graphics g, Entity e);
 
+	/**
+	 * if the entity has more than one image this methods change entity's image
+	 * 
+	 * @param e 		is the {@link Entity} that need to change his image
+	 * @param strImg	is the path of the new image
+	 */
 	public void switchImage(Entity e, String strImg);
 	
 }
