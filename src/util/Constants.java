51648178e66cdbb1406c0c329e61c8f91e2f8db7
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-04-24 11:03:29 +0200

    Modified Constants.java

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -8,19 +8,19 @@ import java.awt.Dimension;
  */
 public interface Constants {
 	
-	Dimensions screenInfo = new Dimensions();
-	Dimension preferDimension = new Dimension(screenInfo.getPreferScreenWidth(), screenInfo.getPreferScreenHeight());
-	Dimension maxLabelDimension = new Dimension(screenInfo.getMaxLabelWidth(), screenInfo.getMaxLabelHeight());
-	int titleSize = 60;
-	int subtitleSize = 25;
-	int minPodium = 1;
-	int maxPodium = 3;
-	Color colorTitle = Color.cyan;
-	Color colorSubtitle = Color.white;
-	boolean IN_LOOP = true;
-	boolean NOT_IN_LOOP = false;
-	float VOLUME_LEVEL_START = 7f;
-	int SPACING = 1;
-	int minSliderValue = 0;
-	int maxSliderValue = 10;
+	final static Dimensions screenInfo = new Dimensions();
+	final static Dimension preferDimension = new Dimension(screenInfo.getPreferScreenWidth(), screenInfo.getPreferScreenHeight());
+	final static Dimension maxLabelDimension = new Dimension(screenInfo.getMaxLabelWidth(), screenInfo.getMaxLabelHeight());
+	final static int titleSize = 60;
+	final static int subtitleSize = 25;
+	final static int minPodium = 1;
+	final static int maxPodium = 3;
+	final static Color colorTitle = Color.cyan;
+	final static Color colorSubtitle = Color.white;
+	final static boolean IN_LOOP = true;
+	final static boolean NOT_IN_LOOP = false;
+	final static float VOLUME_LEVEL_START = 0.7f;
+	final static int SPACING = 1;
+	final static int minSliderValue = 0;
+	final static int maxSliderValue = 10;
 }
