37f99091a8b4ba04b4a91718c9878e44b4464bef
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-04-24 11:02:53 +0200

    Modified AudioImpl.java

--- src/util/AudioImpl.java
+++ src/util/AudioImpl.java
@@ -1,6 +1,10 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
@@ -20,12 +24,21 @@ public class AudioImpl implements Audio{
 	private AudioInputStream audioIn;
 	private float gain;
 	private float volume;
+	private BufferedReader reader;
+	private BufferedWriter writer;
 
 	/**
 	 * The constructor creates a new Audio for each audio track.
 	 */
     public AudioImpl() {
-    	this.volume = Constants.VOLUME_LEVEL_START;
+    	try {
+			this.reader = new BufferedReader(new FileReader(Strings.VOLUME_URI));
+			String level = this.reader.readLine().strip();
+			this.volume = Float.parseFloat(level);
+		} catch (IOException | NullPointerException e1) {
+			this.volume = Constants.VOLUME_LEVEL_START;
+			System.out.println("error in the sound files");
+		}
     }
     
 	@Override
@@ -36,6 +49,7 @@ public class AudioImpl implements Audio{
 				this.clip = AudioSystem.getClip();
 				this.clip.open(audioIn);
 				this.clip.start();
+				this.setVolume(this.volume);
 				if(inLoop) {
 					this.clip.loop(Clip.LOOP_CONTINUOUSLY);
 				}
@@ -54,13 +68,20 @@ public class AudioImpl implements Audio{
 			FloatControl floatControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 			float range = floatControl.getMaximum() - floatControl.getMinimum();
 			this.gain = (range * volume) + floatControl.getMinimum();
+			this.volume = volume;
 			floatControl.setValue(gain);
+			try {
+				this.writer = new BufferedWriter(new FileWriter(Strings.VOLUME_URI));
+				this.writer.write("" + this.getVolume());
+				this.writer.close();
+			} catch (IOException e1) {
+				System.out.println("can't find the volume file");
+			}
 	}
 
 	@Override
 	public float getVolume() {
 		return this.volume;
 	}
-
-
+	
 }
