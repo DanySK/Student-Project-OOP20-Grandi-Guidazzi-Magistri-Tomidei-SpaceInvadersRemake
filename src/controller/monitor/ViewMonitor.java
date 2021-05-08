9d45e358d60da181623bff327535d1d5863831a2
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-05-08 14:51:22 +0200

    created an enumeration for settings the status of the game controller

---
+++ src/controller/monitor/ViewMonitor.java
@@ -0,0 +1,25 @@
+package controller.monitor;
+
+import controller.GameStatus;
+
+/**
+ * Interface for set the {@link GameStatus}
+ */
+public interface ViewMonitor {
+
+	/**
+	 * Set the {@link GameStatus} equal to paused
+	 */
+	public void pause();
+
+	/**
+	 * Set the {@link GameStatus} equal to resumed
+	 */
+	public void resume();
+
+	/**
+	 * Set the {@link GameStatus} equal to stopped
+	 */
+	public void stop();
+
+}
