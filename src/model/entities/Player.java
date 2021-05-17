449c77f16cb24dc7c5afc575fd1e2ea640076466
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-05-17 12:17:47 +0200

    Add some changes to the Player class

--- src/model/entities/Player.java
+++ src/model/entities/Player.java
@@ -1,39 +1,33 @@
 package model.entities;
 
-import java.awt.Graphics;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.List;
-
-import graphics.EntityGraphics;
-import menu.Board;
 import model.entitiesutil.EntityDirections;
 import model.entitiesutil.GenericEntityType;
-import model.entitiesutil.typeentities.MobileEntity;
+import model.entitiesutil.typeentities.GenericEntity;
+import model.entitiesutil.typeentities.UserEntity;
+import model.physics.EntityCollision.EdgeCollision;
 import model.physics.EntityMovement;
 import util.Pair;
 
-public class Player{
+public class Player implements UserEntity{
 
-	private GenericEntityType entityType;
-	private Pair<Integer,Integer> position;
-	private int movimentUnitX;
-	private int movimentUnitY;
+	private SpecificEntityType entityType;
+	private Pair<Double,Double> position;
+	private EntityDirections direction;
+	private double movimentUnitX;
+	private double movimentUnitY;
 	private int height;
 	private int width;
 	private int hit;
 	private int maxHits;
-	private List<String> strImages;
-	private EntityGraphics graphics;
 	private EntityMovement move;
 	
 	/**
 	 * The Constractor that create the Player.
 	 * @param position
 	 */
-	public Player(Pair<Integer,Integer> position) {
-		this.create(position, this.movimentUnitX, this.movimentUnitY, this.height, this.width, this.hit, 
-				this.maxHits, this.strImages, this.graphics, this.move);
+	public Player(int x, int y) {
+		this.create(x, y, this.movimentUnitX, this.movimentUnitY, this.height, this.width, 3, 
+				this.move);
 	}
 	
 	/**
@@ -50,114 +44,142 @@ public class Player{
 	 * @param move
 	 * @param dir
 	 */
-	private void create(Pair<Integer,Integer> pos, int movimentUnitX2, int movimentUnitY2, int height, int width, int hit, int maxHits, 
-			List<String> listImages, EntityGraphics graphics, EntityMovement move) {
-		this.entityType = GenericEntityType.PLAYER;
-		this.position = new Pair<>(pos.getX(), pos.getY());
+	private void create(int x, int y, double movimentUnitX2, double movimentUnitY2, 
+			int height, int width, int maxHits, EntityMovement move) {
+		this.entityType = SpecificEntityType.PLAYER_1;
+		this.position = new Pair<>((double)x, (double)y);
 		this.movimentUnitX = movimentUnitX2;
 		this.movimentUnitY = movimentUnitY2;
 		this.height = height;
 		this.width = width;
 		this.hit = 0;
 		this.maxHits = maxHits;
-		this.graphics = graphics;
 		this.move = move;
+		
 	}
 	
-	
-	public Pair<Integer, Integer> getPos() {
+	public Pair<Double, Double> getPos() {
 		return this.position;
 	}
 
-	
 	public void setPos(Pair<Integer, Integer> pos) {
 		pos.setBoth(pos.getX(), pos.getY());
 	}
 
-	
-	public int getX() {
-		return this.position.getX();
-	}
-
-	
-	public int getY() {
-		return this.position.getY();
-	}
-
-	
-	public void setX(int x) {
-		this.position.setX(x);
-	}
-
-	
-	public void setY(int y) {
-		this.position.setY(y);
-	}
-
-	
 	public int getWidth() {
 		return this.width;
 	}
 
-	
 	public int getHeight() {
 		return this.height;
 	}
 
+	public boolean isAlive() {
+		return this.hit < this.maxHits;
+	}
 	
-	public int getMuX() {
+	public void incHits() {
+		this.hit++;
+	}
+
+	@Override
+	public double getMuX() {
 		return this.movimentUnitX;
 	}
 
-	
-	public void setMuX(int mux) {
-		this.movimentUnitX = mux;
+	@Override
+	public void setMuX(double mux) {
+		this.movimentUnitY = mux;
 	}
 
-	
+	@Override
+	public void setMuY(double muy) {
+		this.movimentUnitY = muy;
+	}
+
+	@Override
+	public EntityDirections getDirection() {
+		return this.direction;
+	}
+
+	@Override
+	public void setDirection(EntityDirections direction) {
+		this.direction = direction;
+	}
+
+	@Override
+	public void doAfterCollisionWithEdge(EdgeCollision edge) {
+		if(edge.equals(EdgeCollision.LEFT) || edge.equals(EdgeCollision.RIGHT)) {
+			this.setPos(new Pair<>((int)this.getX(), (int)this.getY()));
+		}
+	}
+
+	@Override
+	public double getX() {
+		return this.position.getX();
+	}
+
+	@Override
+	public double getY() {
+		return this.position.getY();
+	}
+
+	@Override
+	public void setX(double x) {
+		this.position.setY(x);
+		
+	}
+
+	@Override
+	public void setY(double y) {
+		this.position.setY(y);
+		
+	}
+
+	@Override
+	public void doAfterCollisionWithEntity(GenericEntity entity) {
+		if((entity.getEntityType().equals(SpecificEntityType.ALIEN_BULLET) || entity.getEntityType().equals(SpecificEntityType.BOSS_1_BULLET)
+				|| entity.getEntityType().equals(SpecificEntityType.BOSS_2_BULLET) || entity.getEntityType().equals(SpecificEntityType.BOSS_3_BULLET))
+				&& this.isAlive()) {
+			this.incHits();
+		} else if( !this.isAlive()) {
+			//this.model.processGameOver();
+		}
+	}
+
+	@Override
+	public SpecificEntityType getEntityType() {
+		return this.entityType;
+	}
+
+	@Override
+	public void shoot() {
+		/*this.model.getNewEntitiesLvel().add(new MonoDirectionPlayerBullet(this.getX(), getY() - this.height/2),
+				SpecificEntityType.PLAYER_BULLET);*/
+		
+	}
+
+
+	@Override
 	public double getMuY() {
 		return this.movimentUnitY;
 	}
 
-	
-	public void setMuY(int muy) {
-		this.movimentUnitY = muy;
+	@Override
+	public void setPos(int x, int y) {
+		this.position.setBoth((double)x-this.width/2, (double)y-this.height/2);
+		
 	}
 
-	
-	public boolean isAlive() {
-		return this.hit >= this.maxHits;
-	}
-
-
-	
-	public EntityMovement getMovementImpl() {
-		return this.move;
-	}
-
-
-	
-	public void updateEntity(Graphics g, MobileEntity e) {
-		this.graphics.updateGraphics(g, e);
-	}
-
-
-
-	public GenericEntityType getEntityType() {
-		return this.entityType;
-	}
-
-	
-	public void setEntityStrImgs(List<String> newEntityStrImg) {
+	@Override
+	public void updateEntityPosition(EntityDirections direction) {
+		if(this.getDirection().equals(EntityDirections.LEFT)) {
+			this.setX(this.getX() - this.getMuX());
+		} else {
+			this.setX(this.getX() + this.getMuX());
+			}
+		this.setX(this.getX() + this.getMuX());
+		}
 		
 	}
 	
-	public void moveRight() {
-		this.setX(this.getX() + this.getMuX()); //setMuX(5)
-	}
-	
-	public void moveLeft() {
-		this.setX(this.getX() - this.getMuX());//setMux(-5)
-	}
-	
-}
