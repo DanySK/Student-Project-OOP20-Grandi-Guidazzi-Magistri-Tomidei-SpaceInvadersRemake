75df4161ed04221d87b8ed6316cd2cceb5a3164b
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-05-24 18:12:54 +0200

    changed monitor implementation

--- src/controller/monitor/ViewMonitor.java
+++ src/controller/monitor/ViewMonitor.java
@@ -8,17 +8,22 @@ import controller.GameStatus;
 public interface ViewMonitor {
 
 	/**
-	 * Set the {@link GameStatus} equal to paused
+	 * pause the game loop if the game is running
 	 */
 	public void pause();
 
 	/**
-	 * Set the {@link GameStatus} equal to resumed
+	 * resume the game loop if the game is paused
 	 */
 	public void resume();
 
 	/**
-	 * Set the {@link GameStatus} equal to stopped
+	 * restart the game loop if the game is paused
+	 */
+	public void restart();
+
+	/**
+	 * stop the game loop
 	 */
 	public void stop();
 
