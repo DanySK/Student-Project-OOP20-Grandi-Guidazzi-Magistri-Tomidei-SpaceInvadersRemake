d053102ca8849648d83eb55aa325038e0605c7be
unknown <federico.guidazzi@studio.unibo.it>
2021-05-24 16:08:31 +0200

    change the interfaces into classes and modified the fields

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -6,25 +6,25 @@ import java.awt.Dimension;
 /**
  * An Interface to store all the constants in the project.
  */
-public interface Constants {
+public class Constants {
 	
-	final static Dimensions screenInfo = new Dimensions();
-	final static Dimension preferDimension = new Dimension(screenInfo.getPreferScreenWidth(), screenInfo.getPreferScreenHeight());
-	final static Dimension maxLabelDimension = new Dimension(screenInfo.getMaxLabelWidth(), screenInfo.getMaxLabelHeight());
-	final static Dimension maxButtonDimension = new Dimension(screenInfo.getMaxLabelWidth(), screenInfo.getMaxButtonHeight());
-	final static Dimension maxTextFieldDimension = new Dimension(screenInfo.getMaxTextFieldWidth(), screenInfo.getMaxTextFieldHeight());
-	final static int titleSize = 60;
-	final static int subtitleSize = 25;
-	final static int minPodium = 1;
-	final static int maxPodium = 3;
-	final static Color colorTitle = Color.cyan;
-	final static Color colorSubtitle = Color.white;
-	final static boolean IN_LOOP = true;
-	final static boolean NOT_IN_LOOP = false;
-	final static float VOLUME_LEVEL_START = 0.7f;
-	final static int SPACING = 1;
-	final static int minSliderValue = 0;
-	final static int maxSliderValue = 10;
+	public final static Dimensions screenInfo = new Dimensions();
+	public final static Dimension preferDimension = new Dimension(screenInfo.getPreferScreenWidth(), screenInfo.getPreferScreenHeight());
+	public final static Dimension maxLabelDimension = new Dimension(screenInfo.getMaxLabelWidth(), screenInfo.getMaxLabelHeight());
+	public final static Dimension maxButtonDimension = new Dimension(screenInfo.getMaxLabelWidth(), screenInfo.getMaxButtonHeight());
+	public final static Dimension maxTextFieldDimension = new Dimension(screenInfo.getMaxTextFieldWidth(), screenInfo.getMaxTextFieldHeight());
+	public final static int titleSize = 60;
+	public final static int subtitleSize = 25;
+	public final static int minPodium = 1;
+	public final static int maxPodium = 3;
+	public final static Color colorTitle = Color.cyan;
+	public final static Color colorSubtitle = Color.white;
+	public final static boolean IN_LOOP = true;
+	public final static boolean NOT_IN_LOOP = false;
+	public final static float VOLUME_LEVEL_START = 0.7f;
+	public final static int SPACING = 1;
+	public final static int minSliderValue = 0;
+	public final static int maxSliderValue = 10;
 	
-	final static int maxLeaderboardCharacters = 10; 
+	public final static int maxLeaderboardCharacters = 10; 
 }
--- src/util/Strings.java
+++ src/util/Strings.java
@@ -3,37 +3,38 @@ package util;
 /**
  * An interface to store all the project's strings.
  */
-public interface Strings {
+public class Strings {
 	
-	String ABOUT_TEXT = "<html><center>The original videogame was an arcade created in 1978, by Tomohiro Nishikado." +
+	public final static String ABOUT_TEXT = "<html><center>The original videogame was an arcade created in 1978, by Tomohiro Nishikado." +
 						"<br/> The game was inspired by the videogame \"Space Monster\" and from the novel \"The War of the Worlds\"." +
 						"<br/> The game was so popular that than over the years was adapted for all the platforms avaiable.</center></html>";
 	
-	String MORE_INFO_LINK = "https://en.wikipedia.org/wiki/Space_Invaders";
+	public final static String MORE_INFO_LINK = "https://en.wikipedia.org/wiki/Space_Invaders";
 	
-	String LEADERBOARD_URI = "src/res/Leaderboard.txt";
-	String LEADERBOARD_DEFAULT_TEXT = "No one is that good yet";
+	public final static String LEADERBOARD_URI = "src/res/Leaderboard.txt";
+	public final static String LEADERBOARD_DEFAULT_TEXT = "No one is that good yet";
 	
-	String VOLUME_URI = "src/res/Volume.txt";
+	public final static String VOLUME_URI = "src/res/Volume.txt";
 	
-	String PANEL_BACKGROUND = "src/res/backgroundImage.png";
-	String GAME_OVER_BACKGROUND = "src/res/gameOverBackground.png";
+	public final static String PANEL_BACKGROUND = "src/res/backgroundImage.png";
+	public final static String GAME_OVER_BACKGROUND = "src/res/gameOverBackground.png";
+	public final static String WIN_BACKGROUND = "src/res/winBackground.png";
 	
-	String ABOUT = "About the original game...";
-	String MORE_INFO = "For more info, click here!";
-	String CREDITS = "Credits";
-	String LEADERBOARD = "Leaderboard";
-	String START = "Start";
-	String OPTIONS = "Options";
-	String EXIT = "Exit";
-	String CHANGE_KEYS = "Change keys";
-	String AUDIO_SETTINGS = "Audio settings";
-	String GO_BACK_TO_MENU = "Go back to menu";
-	String RESTART = "Restart game";
-	String RESUME = "Resume Game";
-	String RETURN_TO_GAME_MENU = "Return back to game menu";
-	String AUDIO_SETTINGS_IN_GAME = "Game audio settings";
+	public final static String ABOUT = "About the original game...";
+	public final static String MORE_INFO = "For more info, click here!";
+	public final static String CREDITS = "Credits";
+	public final static String LEADERBOARD = "Leaderboard";
+	public final static String START = "Start";
+	public final static String OPTIONS = "Options";
+	public final static String EXIT = "Exit";
+	public final static String CHANGE_KEYS = "Change keys";
+	public final static String AUDIO_SETTINGS = "Audio settings";
+	public final static String GO_BACK_TO_MENU = "Go back to menu";
+	public final static String RESTART = "Restart game";
+	public final static String RESUME = "Resume Game";
+	public final static String RETURN_TO_GAME_MENU = "Return back to game menu";
+	public final static String AUDIO_SETTINGS_IN_GAME = "Game audio settings";
 	
-	String SOUNDS_DIRECTORY = "src/res/";
+	public final static String SOUNDS_DIRECTORY = "src/res/";
 	
 }
