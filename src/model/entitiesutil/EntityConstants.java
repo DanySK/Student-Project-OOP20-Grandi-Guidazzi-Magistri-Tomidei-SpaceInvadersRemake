e4cc73be33dd3ccd964c72693c7e394384f6d545
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-06-01 16:01:58 +0200

    Create the class for the entity constants

---
+++ src/model/entitiesutil/EntityConstants.java
@@ -0,0 +1,14 @@
+package model.entitiesutil;
+
+public class EntityConstants {
+
+	public static class PlayerConstants {
+		
+		
+		public final static int INITIAL_WIDTH = 0;
+		public final static int INITIAL_HEIGHT = 0;
+		public final static double INITIAL_MU_X = 0;
+		public final static double INITIAL_MU_Y = 0;
+		public final static int  MAX_HITS = 3;
+	}
+}
