c45d106a7de1066fe1bafb37926f5313f55c73a5
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-06-10 21:01:01 +0200

    Add the comments for each method of the class

--- src/model/entities/MonoDirectionPlayerBullet.java
+++ src/model/entities/MonoDirectionPlayerBullet.java
@@ -1,20 +1,18 @@
 package model.entities;
 
 import model.entitiesutil.Bullet;
+import model.entitiesutil.EntityConstants;
 import model.entitiesutil.EntityDirections;
 import model.entitiesutil.GenericEntityType;
 import model.entitiesutil.typeentities.GenericEntity;
 import model.physics.EntityMovementImpl;
 
 public class MonoDirectionPlayerBullet extends Bullet{
-
-	private final int BULLET_INITIAL_WIDTH = 0;
-	private final int BULLET_INITIAL_HEIGHT = 0;
-	private final double BULLET_MAX_MU_Y = 0;
 	
 	public MonoDirectionPlayerBullet(double x, double y) {
-		this.create(SpecificEntityType.PLAYER_1_BULLET, BULLET_MAX_MU_Y, BULLET_MAX_MU_Y, BULLET_INITIAL_WIDTH, BULLET_INITIAL_HEIGHT, 
-				BULLET_MAX_MU_Y, BULLET_MAX_MU_Y, EntityDirections.UP, new EntityMovementImpl());
+		this.create(SpecificEntityType.PLAYER_1_BULLET, x, y, EntityConstants.PlayerBullet.PLAYER_BULLET_INITIAL_WIDTH, 
+				EntityConstants.PlayerBullet.PLAYER_BULLET_INITIAL_HEIGHT, EntityConstants.PlayerBullet.PLAYER_BULLET_MU_X, 
+				EntityConstants.PlayerBullet.PLAYER_BULLET_MU_Y, EntityDirections.UP, new EntityMovementImpl());
 	}
 
 	@Override
