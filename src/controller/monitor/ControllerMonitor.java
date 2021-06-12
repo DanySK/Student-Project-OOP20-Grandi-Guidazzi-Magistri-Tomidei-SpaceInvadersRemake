26ea9a67e990fdaae2a288528291f4af3c366d85
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-06-12 20:09:12 +0200

    removed unused methods

--- src/controller/monitor/ControllerMonitor.java
+++ src/controller/monitor/ControllerMonitor.java
@@ -1,9 +1,28 @@
 package controller.monitor;
 
+import controller.GameStatus;
+
 /**
  * Interface to monitor the status of the {@link GameController}
  */
 public interface ControllerMonitor {
+
+	/**
+	 * Set the {@link GameStatus} equal to stopped
+	 */
+	public void stop();
+
+	/**
+	 * Puts the thread that call this method to wait if the game status is equal to paused
+	 */
+	public void isGamePaused();
+
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	public GameStatus getGameStatus();
 
 	/**
 	 * Set the game status equal to running after a game resume
@@ -14,4 +33,5 @@ public interface ControllerMonitor {
 	 * Set the game status equal to running
 	 */
 	public void setStart();
+
 }
