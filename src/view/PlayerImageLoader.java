334152b2b204bedf7f70345cfad605eff18d627a
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-06-13 18:56:43 +0200

    The class that manage the loading of player images

--- src/view/PlayerImageLoader.java
+++ src/view/PlayerImageLoader.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Image;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,11 +10,12 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
-import model.entities.SpecificEntityType;
 import model.entitiesutil.EntityConstants;
-import model.entitiesutil.typeentities.GenericEntity;
 import util.Strings;
 
+/**
+ * A class that allows to load the image for the player.
+ */
 public class PlayerImageLoader {
 	ArrayList<String> playerImageList = new ArrayList<>();
 	Random randomPlayerImage = new Random();
@@ -25,24 +27,44 @@ public class PlayerImageLoader {
 		this.playerImageList.add(Strings.NOSE_SKIN);
 	}
 	
+	/**
+	 * The method that allows to choose the image if the user choose one of the four playerSkin.
+	 * @param playerImage
+	 * @return
+	 * @throws IOException
+	 */
 	public Optional<Image> choseImage(String playerImage) throws IOException{
 		
+		final StringBuilder stringNameBuilder = new StringBuilder();
+		stringNameBuilder.append(playerImage);
 		Optional<Image> image = Optional.empty();
+		
 		if(this.playerImageList.contains(playerImage)) {
-			image = Optional.of(ImageIO.read(new File(playerImage)));
-			return this.resizeImage(image.get(), EntityConstants.PlayerConstants.INITIAL_HEIGHT, EntityConstants.PlayerConstants.INITIAL_HEIGHT);
+			image = Optional.of(ImageIO.read(new File(stringNameBuilder.toString())));
+			return this.resizeImage(image.get(), EntityConstants.Player.PLAYER_INITIAL_WIDTH, EntityConstants.Player.PLAYER_INITIAL_HEIGHT);
 		}
 		return image;
 	}
 	
+	/**
+	 * The method that resized the image.
+	 * @param image
+	 * @param width
+	 * @param heigth
+	 * @return
+	 */
 	private Optional<Image> resizeImage(Image image, int width, int heigth) {
 		return Optional.of(image.getScaledInstance(width, heigth, Image.SCALE_DEFAULT));
 	}
 	
-	public Optional<Image> selectRandomSkin() throws IOException {
+	/**
+	 * The method select randomly one of the four playerImage. 
+	 * @throws IOException
+	 */
+	public void selectRandomSkin() throws IOException {
 		int randomItem = this.randomPlayerImage.nextInt(this.playerImageList.size());
 		String chooseItem = this.playerImageList.get(randomItem);
 		Image randomImage = ImageIO.read(new File(chooseItem));
-		return this.resizeImage(randomImage, EntityConstants.PlayerConstants.INITIAL_HEIGHT, EntityConstants.PlayerConstants.INITIAL_WIDTH);
+		this.resizeImage(randomImage, EntityConstants.Player.PLAYER_INITIAL_WIDTH, EntityConstants.Player.PLAYER_INITIAL_HEIGHT);
 	}
 }
