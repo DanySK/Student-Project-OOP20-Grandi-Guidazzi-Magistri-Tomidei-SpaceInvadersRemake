aba21bf6c1c689f88b072d2dd430723635224b4e
unknown <federico.guidazzi@studio.unibo.it>
2021-06-13 18:40:10 +0200

    bug fixing

--- src/model/entitiesutil/EntityConstants.java
+++ src/model/entitiesutil/EntityConstants.java
@@ -3,17 +3,24 @@ package model.entitiesutil;
 public class EntityConstants { 
 	
 	public class PlayerBullet{
-		public final static int PLAYER_BULLET_MU_X = 1;
-		public final static int  PLAYER_BULLET_INITIAL_WIDTH = 0;
-		public final static int  PLAYER_BULLET_INITIAL_HEIGHT = 0;
-		public final static double  PLAYER_BULLET_MU_Y = 0;
+		public final static int INITIAL_MU_X = 1;
+		public final static int  INITIALWIDTH = 0;
+		public final static int  INITIALHEIGHT = 0;
+		public final static double  INITIAL_MU_Y = 0;
 	}
 		
 	public class Player{
-		public final static int PLAYER_INITIAL_WIDTH = 0;
-		public final static int PLAYER_INITIAL_HEIGHT = 0;
-		public final static double PLAYER_INITIAL_MU_X = 0;
-		public final static double PLAYER_INITIAL_MU_Y = 0;
-		public final static int PLAYER_MAX_HITS = 3;
+		public final static int INITIAL_WIDTH = 0;
+		public final static int INITIAL_HEIGHT = 0;
+		public final static double INITIAL_MU_X = 0;
+		public final static double INITIAL_MU_Y = 0;
+		public final static int MAX_HITS = 3;
+	}
+	
+	public class Alien{
+		public final static int INITIAL_WIDTH = 0;
+		public final static int INITIAL_HEIGHT = 0;
+		public final static double INITIAL_MU_X = 0;
+		public final static double INITIAL_MU_Y = 0;
 	}
 }
