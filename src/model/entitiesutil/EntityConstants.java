6530de67ad94855aaa5652029e1c04689e4b3cd5
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-06-19 13:06:38 +0200

    for each entity that can fire automatically, added a new constant representing how many cycles it has to wait to fire

--- src/model/entitiesutil/EntityConstants.java
+++ src/model/entitiesutil/EntityConstants.java
@@ -31,15 +31,16 @@ public class EntityConstants {
 		public final static double INITIAL_MU_X = 0;
 		public final static double INITIAL_MU_Y = 0;
 		public final static int MAX_HITS = 0;
+		public final static int CYCLES_TO_SHOOT = 0; 
 	}
 
 	public class Boss2 {
 		public final static int INITIAL_WIDTH = 0;
 		public final static int INITIAL_HEIGHT = 0;
-		public final static double INITIAL_MU_X = 0;
 		public final static double INITIAL_MU_Y = 0;
 		public final static int HITS_2ND_PHASE = 0;
 		public final static int MAX_HITS = 0;
+		public final static int CYCLES_TO_SHOOT = 0;
 	}
 
 	public class Boss3 {
@@ -50,6 +51,8 @@ public class EntityConstants {
 		public final static int MAX_HITS = 0;
 		public final static int MAX_SPEED = 0;
 		public final static int HITS_2ND_PHASE = MAX_HITS/2;
+		public final static int CYCLES_TO_SHOOT_1ST_PHASE = 0;
+		public final static int CYCLES_TO_SHOOT_2ND_PHASE = 0;
 	}
 
 	public class MonoDirectionEnemyBullet {
