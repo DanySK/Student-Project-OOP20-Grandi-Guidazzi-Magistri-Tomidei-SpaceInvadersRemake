5b4dac6af1395bf41c567b54f5e758b32fde295b
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-06-23 14:22:21 +0200

    Remamed the class GraphicsView Impl to GraphicsView

--- src/view/GraphicsView.java
+++ src/view/GraphicsView.java
@@ -1,16 +1,47 @@
 package view;
 
-import java.util.Set;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.IOException;
 
-import model.entitiesutil.typeentities.GenericEntity;
+import javax.swing.JPanel;
 
-public interface GraphicsView {
+import controller.ViewGameController;
 
-	//public void updateEntityImages();
+/**
+ * A class that manages the view.
+ */
+public class GraphicsView extends JPanel{
 
-	/**
-	 * The method that update the images.
-	 */
-	void updateEntityImages(Set<GenericEntity> entitySet);
+	private static final long serialVersionUID = 1L;
+	private UpdateManager imageManager;
+	private final ViewGameController controller;
 	
+	/**
+	 * The constructor that create the objects to manage the images.
+	 */
+	public GraphicsView(String uriSkin, ViewGameController ctrl) throws IOException{
+		this.controller = ctrl;
+		this.imageManager = new ImageManagerImpl(uriSkin);
+		this.setBackground(Color.BLACK);
+	}
+
+	@Override
+	public void paintComponent(Graphics graphics) {
+		super.paintComponent(graphics);
+		Graphics g2D = (Graphics2D)graphics;
+		
+		this.controller.getLevelEntities().forEach(e -> {
+			Image image;
+			try {
+				image = this.imageManager.drawEntity(e);
+				g2D.drawImage(image, (int)e.getX()-e.getWidth()/2, (int)e.getY()-e.getHeight()/2, null);
+			} catch (IOException e1) {
+				System.exit(1);
+			}
+		});
+	}
+
 }
