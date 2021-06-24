1668488dc47815ca97d5d9b7a52d0a4ef7dbd2af
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-06-24 19:44:28 +0200

    added a new implementation of player

--- src/model/entitiesutil/Player.java
+++ src/model/entitiesutil/Player.java
@@ -5,7 +5,7 @@ import model.entitiesutil.typeentities.UserEntity;
 import util.Pair;
 
 /**
- * The entity with which the user can play.
+ * A class that create the player entity.
  */
 public abstract class Player implements UserEntity{
 
@@ -18,154 +18,96 @@ public abstract class Player implements UserEntity{
 	private int hit;
 	private int maxHits;
 	
-	/**
-	 * Method that generate the entity player.
-	 * @param type		is the {@link SpecificEntityType}
-	 * @param x			is the initial x coordinate of the {@link Player}
-	 * @param y			is the initial y coordinate of the {@link Player}
-	 * @param width		is the initial width of the {@link Player}
-	 * @param height	is the initial height of the {@link Player}
-	 * @param muX		is the initial movement unit of the {@link Player} along x-axis
-	 * @param muY		is the initial movement unit of the {@link Player} along y-axis
-	 * @param maxHits	is the max number of hits that {@link Player} can take before dying
-	 */
-	public void create(SpecificEntityType type, double x, double y, int width,int height, 
+	protected void create(SpecificEntityType type, double x, double y, int width,int height, 
 			double muX, double muY, int maxHits) {
-		this.entityType = SpecificEntityType.PLAYER_1;
+		this.width = width;
+		this.height = height;
 		this.position = new Pair<>(x, y);
 		this.movimentUnitX = muX;
 		this.movimentUnitY = muY;
-		this.height = height;
-		this.width = width;
 		this.hit = 0;
 		this.maxHits = maxHits;
+		this.entityType = type;
 	}
-	
-	/**
-	 * A method that returns the position of player in a specific moment.
-	 */
+
+	@Override
 	public Pair<Double, Double> getPos() {
 		return this.position;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public void setPos(double x, double y) {
 		this.position.setBoth(x, y);
 	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int getWidth() {
-		return this.width;
-	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int getHeight() {
-		return this.height;
-	}
-    
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean isAlive() {
-		return this.hit < this.maxHits;
-	}
-	
-	/**
-	 * Method that increments the number of hit.
-	 */
-	public void incHits() {
-		this.hit++;
-	}
-
-	/**
-	 * Method that returns the number of hit suffered by the player.
-	 * @return
-	 */
-	public int getHit() {
-		return this.hit;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public double getMuX() {
-		return this.movimentUnitX;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setMuX(double mux) {
-		this.movimentUnitY = mux;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setMuY(double muy) {
-		this.movimentUnitY = muy;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public double getX() {
 		return this.position.getX();
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public double getY() {
 		return this.position.getY();
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public void setX(double x) {
-		this.position.setX(x);
+		this.position.setX(x);		
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public void setY(double y) {
 		this.position.setY(y);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
+	@Override
+	public int getWidth() {
+		return this.width;
+	}
+
+	@Override
+	public int getHeight() {
+		return this.height;
+	}
+
+	@Override
+	public boolean isAlive() {
+		return this.hit < this.maxHits;
+	}
+
+	protected void incHits() {
+		this.hit++;
+	}
+
+	protected int getHits() {
+		return this.hit;
+	}
+
+	@Override
+	public double getMuX() {
+		return this.movimentUnitX;
+	}
+
+	@Override
+	public void setMuX(double mux) {
+		this.movimentUnitX = mux;
+	}
+
+	@Override
+	public double getMuY() {
+		return this.movimentUnitY;
+	}
+
+	@Override
+	public void setMuY(double muy) {
+		this.movimentUnitY = muy;
+	}
+
 	@Override
 	public SpecificEntityType getEntityType() {
 		return this.entityType;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public double getMuY() {
-		return this.movimentUnitY;
-	}
-		
+	public abstract void shoot();
 }
 	
