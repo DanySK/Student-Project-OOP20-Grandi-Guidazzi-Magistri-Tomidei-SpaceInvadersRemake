89bd42c32b695845d1e965f7980663efde89ab17
unknown <federico.guidazzi@studio.unibo.it>
2021-06-25 14:56:46 +0200

    change the time for shoot and the life of the bosses

--- src/model/entitiesutil/EntityConstants.java
+++ src/model/entitiesutil/EntityConstants.java
@@ -18,7 +18,7 @@ public class EntityConstants {
 		public final static int INITIAL_HEIGHT = 90;
 		public final static double INITIAL_MU_X = 10;
 		public final static int MAX_HITS = 3;
-		public final static int CYCLES_TO_SHOOT = 5; 
+		public final static int CYCLES_TO_SHOOT = 15; 
 	}
 	
 	public class Alien{
@@ -27,7 +27,7 @@ public class EntityConstants {
 		public final static double INITIAL_MU_X = 1;
 		public final static double INITIAL_MU_Y = 10;
 		public final static int MAX_HIT = 1;
-		public final static int CYCLES_TO_SHOOT = 80; 
+		public final static int CYCLES_TO_SHOOT = 100; 
 	}
 
 	public class Boss1 {
@@ -35,7 +35,7 @@ public class EntityConstants {
 		public final static int INITIAL_HEIGHT = 120;
 		public final static double INITIAL_MU_X = 1.2;
 		public final static double INITIAL_MU_Y = 20;
-		public final static int MAX_HITS = 10;
+		public final static int MAX_HITS = 20;
 		public final static int CYCLES_TO_SHOOT = 72; 
 	}
 
@@ -43,7 +43,7 @@ public class EntityConstants {
 		public final static int INITIAL_WIDTH = 300;
 		public final static int INITIAL_HEIGHT = 200;
 		public final static double INITIAL_MU_Y = 0.6;
-		public final static int MAX_HITS = 20;
+		public final static int MAX_HITS = 25;
 		public final static int HITS_2ND_PHASE = MAX_HITS/2;
 		public final static int CYCLES_TO_SHOOT = 80;
 	}
