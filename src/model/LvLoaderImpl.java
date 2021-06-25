4d4ec5f29420974c53eebe96d258f9c32837e8b6
unknown <federico.guidazzi@studio.unibo.it>
2021-06-25 12:51:53 +0200

    removed unused imports

--- src/model/LvLoaderImpl.java
+++ src/model/LvLoaderImpl.java
@@ -14,25 +14,6 @@ public class LvLoaderImpl implements LvLoader{
 	 */
 	@Override
 	public Optional<Level> loadLevel(int levelNumber) {
-		Optional<Level> JSonEntities = Optional.empty();
-//		try {
-//	        // create Gson instance
-//	        Gson gson = new Gson();
-//
-//	        // create a reader
-//	        Reader reader = Files.newBufferedReader(Paths.get("src/res/levels/LV" + levelNumber + ".json"));
-//
-//	        // convert JSON string to Level object
-//	        JSonEntities = Optional.ofNullable(gson.fromJson(reader, Level.class));
-//
-//	        // close reader
-//	        reader.close();
-//
-//	    } catch (Exception ex) {
-//	        ex.printStackTrace();
-//	    }
-//
-//		return JSonEntities;
 		return Optional.ofNullable(new LvImpl(levelNumber));
 	}
 }
