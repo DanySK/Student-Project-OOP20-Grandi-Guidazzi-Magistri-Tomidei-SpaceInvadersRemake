f2aecb8976b500dcbc92df278b667fb10086184f
unknown <federico.guidazzi@studio.unibo.it>
2021-06-25 11:17:26 +0200

    modified the numbers of aliens in the levels

--- src/model/LvImpl.java
+++ src/model/LvImpl.java
@@ -25,18 +25,26 @@ public class LvImpl implements Level{
 	
 	public LvImpl(int levelNum) {
 		switch(levelNum) {
+			case 1:
+				this.bossType = Optional.empty();
+				this.aliens = 25;
+				break;
 			case 2:
 				this.bossType = Optional.ofNullable(SpecificEntityType.BOSS_1.toString());
+				break;
+			case 3:
+				this.bossType = Optional.empty();
+				this.aliens = 30;
 				break;
 			case 4:
 				this.bossType = Optional.ofNullable(SpecificEntityType.BOSS_2.toString());
 				break;
+			case 5:
+				this.bossType = Optional.empty();
+				this.aliens = 35;
+				break;
 			case 6:
 				this.bossType = Optional.ofNullable(SpecificEntityType.BOSS_3.toString());
-				break;
-			default:
-				this.bossType = Optional.empty();
-				this.aliens = 30;
 				break;
 		}
 	}
